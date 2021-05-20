2c8dcf701c947db50b175b99fa2b62e2d75d95a3
Badr Matrane <matrane.badr@gmail.com>
2021-05-20 10:56:44 +0200

    Adding Responsive Navigation & Custom Theme & Logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)},
+  {
+    path:'',component:HomeComponent
+  },
+  { 
+    path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)
+  },
+ 
 ];
 
 @NgModule({
